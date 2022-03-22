ff5bef31e5c1c04ae370b5c47ec33cc98afe65f5
Aldiriansyah Dwi <kurosenpai12@gmail.com>
2022-03-22 19:06:22 +0700

    add direction on main_navigation and finish firstFragment

--- app/src/main/java/com/example/challange3/FirstFragment.kt
+++ app/src/main/java/com/example/challange3/FirstFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.challange3.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
@@ -22,7 +23,9 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+            binding.btnGotoSecond.setOnClickListener {
+                findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+            }
     }
 
     override fun onDestroy() {
